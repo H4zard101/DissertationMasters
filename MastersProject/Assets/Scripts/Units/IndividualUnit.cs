72a96d5ed772af8db9d052a836c07d45febd6b1c
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-02-22 00:01:08 +0000

    Added the knight character as a prefab, messed around with some static formations

---
+++ MastersProject/Assets/Scripts/Units/IndividualUnit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndividualUnit : MonoBehaviour
+{
+
+    [Header("Unit stats")]
+    public int Health = 100;
+    public int Stamina = 100;
+    public int AttackDamage = 30;
+    public float AttackSpeed = 3.0f;
+}
